fba26121b5617b2a84708fd8ef6492e2a53ec896
level077 <level077@163.com>
2016-06-21 15:47:26 +0800

    add etcd_server

--- host2skydns.sh
+++ host2skydns.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-[ $# -ne 2 ] && echo "Usage: sh $0 domain host_file" && exit 1
+[ $# -ne 3 ] && echo "Usage: sh $0 domain host_file etcd_server" && exit 1
 domain=$1
 host_file=$2
+etcd_server=$3
 [ -z $domain ] && echo  "need domain" && exit 1
 [ ! -f $host_file ] && echo "can't find $host_file" && exit 1
+[ -z $etcd_server ] && echo "need ectd server" && exit 1
 while read ip server_name
 do
 	for x in $server_name 
@@ -17,7 +19,7 @@ do
 			do
 				skydns=$skydns/$(echo $x | awk -F '.' "{print \$$i}")
 			done
-			echo curl -XPUT "10.3.1.5:2379/v2/keys/skydns$skydns" -d value=\'{\"host\":\"$ip\"}\'
+			echo curl -XPUT "$etcd_server/v2/keys/skydns$skydns" -d value=\'{\"host\":\"$ip\"}\'
 		fi
 	done
 done < $host_file
