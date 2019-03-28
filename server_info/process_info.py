6dae9c148733b3ffe0839658221b11bbfcefb692
level077 <xxx>
2019-03-28 13:52:56 +0800

    fix

--- server_info/process_info.py
+++ server_info/process_info.py
@@ -7,7 +7,7 @@ software = []
 
 def get_version(name=None,cmdline=None,ports=None):
   version = None
-  with psutil.Popen(["/usr/bin/rpm", '-qf',cmdline], stdout=PIPE,stderr=PIPE) as p:
+  with psutil.Popen(["rpm", '-qf',cmdline], stdout=PIPE,stderr=PIPE) as p:
     p.wait(timeout=1)
     if p.returncode == 0:
       version = p.communicate()[0].strip()
@@ -17,25 +17,25 @@ def get_version(name=None,cmdline=None,ports=None):
       with psutil.Popen([cmdline,"-v"],stdout=PIPE,stderr=PIPE) as p:
         p.wait(timeout=1)
         if p.returncode == 0:
-          version = p.communicate()[0].split("/")[-1]
+          version = p.communicate()[1].split("/")[-1].strip()
           return version
     elif name == "redis-server":
       with psutil.Popen([cmdline,"-v"],stdout=PIPE,stderr=PIPE) as p:
         p.wait(timeout=1)
         if p.returncode == 0:
-          version = p.communicate()[0].split()[2].split("=")[-1]
+          version = p.communicate()[0].split()[2].split("=")[-1].strip()
           return version
     elif name == "etcd":
       with psutil.Popen([cmdline,"-version"],stdout=PIPE,stderr=PIPE) as p:
         p.wait(timeout=1)
         if p.returncode == 0:
-          version = p.communicate()[0].split()[2]
+          version = p.communicate()[0].split()[2].strip()
           return version
     elif name == "mysqld":
       with psutil.Popen([cmdline,"--version"],stdout=PIPE,stderr=PIPE) as p:
         p.wait(timeout=1)
         if p.returncode == 0:
-          version = p.communicate()[0].split()[2]
+          version = p.communicate()[0].split()[2].strip()
           return version
   return "Unknown" 
 
@@ -47,12 +47,15 @@ def get_centos_software():
     s['ports'] = []
     cmdline = proc.info['cmdline']
     if len(cmdline) != 0:
-      s['cmdline'] = cmdline[0].split()[0]
+      if s['name'] == 'nginx':
+        s['cmdline'] = cmdline[3]
+      else:
+        s['cmdline'] = cmdline[0].split()[0]
     for c in proc.info['connections']:
       if c.status == "LISTEN":
         s['ports'].append(c.laddr.port)
         s['listen'].append(c.laddr.ip + ":" + str(c.laddr.port))
-    if len(s['listen']) != 0:
+    if len(s['listen']) != 0 and len(s['cmdline']) !=0:
       s['ports'] = list(set(s['ports']))
       s['version'] = get_version(s['name'],s['cmdline'],s['ports'])
       software.append(s)
