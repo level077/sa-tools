612de038ad9fa69a7e1cd0cba8111a9ba3dd82e5
level077 <xxx>
2019-04-03 14:21:25 +0800

    fix

--- server_info/README.md
+++ server_info/README.md
@@ -2,9 +2,13 @@ server_info.py
 --------------
 * 从ansible set模块中提取出来的脚本，去除了一些信息。
 
+process_info.py
+--------------
+* 获取系统的进程信息及软件信息
+
 send_elastic.py
 --------------
-* 将server_info.py的json输出，发送到elasticsearch集群。
+* 将server_info.py，process_info.py的json输出，发送到elasticsearch集群。
 
 useage
 -------------
---
+++ server_info/send2elastic.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python
+# coding:utf-8
+
+import server_info
+import process_info
+import json
+import sys
+import argparse
+import datetime
+import requests
+
+def send_elastic(body,elastic_host,elastic_port):
+    url = "http://"+ elastic_host + ":" + elastic_port + "/idc/server/"
+    r = requests.post(url, data=body)
+    print(r.status_code)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e","--elastic_host",help="elasticsearch server ip")
+    parser.add_argument("-p","--elastic_port",help="elasticsearch server port")
+    parser.add_argument("-f","--force",action="store_true",help="force update")
+    args = parser.parse_args()
+    if args.elastic_host:
+        elastic_host = args.elastic_host
+    else:
+        print "need elasticsearch host"
+        sys.exit(1)
+    if args.elastic_port:
+        elastic_port = args.elastic_port
+    else:
+        print "need elasticsearch port"
+        sys.exit(1)
+    server_info = server_info.get_result()
+    server_info["process"],server_info["software"] = process_info.get_info()
+    server_info["@timestamp"] = datetime.datetime.utcnow().isoformat()
+    body = json.dumps(server_info)
+    send_elastic(body,elastic_host,elastic_port)
